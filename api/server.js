debe0a9d70edaf1af7f1e1e346327a58779ee9a5
camilalenzi <camila.dduque@gmail.com>
2020-11-24 19:08:05 -0300

    commitatividade

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = 3000;
+const host = 'localhost';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, resp) => {
+
+
+})
